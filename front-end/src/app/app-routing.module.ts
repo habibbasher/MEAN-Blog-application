2494d3479d19c347947ac754d479d5636b940911
habibbasher <habib0904010@gmail.com>
2018-01-09 13:51:47 +0600

    Angular routing implemented and nav, home and dashboard component is created

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+// Importing Custom Components
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+    { 
+        path: '', 
+        component: HomeComponent // The Default Route
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent // The Dashboard Route
+    },
+    { 
+        path: '**', 
+        component: HomeComponent // The "Catch-All" Route
+    }
+  ];
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// Importing Custom Module for routing purposes.
+import { AppRoutingModule } from './app-routing.module';
 
+// Default Component
 import { AppComponent } from './app.component';
+// Custom Components Imported HERE by default when component is created using angular cli
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
